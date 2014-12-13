1e6f36c6066d5684e5a72a941faed91971b4a0c7
vvennava <vikas.vennavali@gmail.com>
2014-12-13 10:19:12 -0800

    JF

---
+++ app/src/main/java/com/vv/buildstuff/joefresh/Miscellaneous.java
@@ -0,0 +1,37 @@
+package com.vv.buildstuff.joefresh;
+
+/**
+ * Created by vvennava on 10/11/14.
+ */
+public enum Miscellaneous {
+
+    NEARBY_SEARCH_URL("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="),
+    KEY("AIzaSyDSeKfurM8gqRX0M4Z8zh0MhOxiqE01Tdo"),
+    TYPES("food|cafe"),
+    DEFAULT_LOCATION("47.673401,-122.342598"),
+    DEFAULT_LATITUDE("47.673401"),
+    DEFAULT_LONGITUDE("-122.342598"),
+    RADIUS("20000"),
+    NAME("Joe Fresh"),
+    DEFAULT_DESTINATION("47.6063683,-122.3322218");
+
+
+    private final String value;
+
+    Miscellaneous(String value) {
+        this.value = value;
+    }
+
+    public String getText() {
+        return value;
+    }
+
+    /* (non-Javadoc)
+         * @see java.lang.Enum#toString()
+         */
+    @Override
+    public String toString() {
+        return value;
+    }
+
+}
---
+++ app/src/main/java/com/vv/buildstuff/joefresh/responsePlaces/Geometry.java
@@ -0,0 +1,26 @@
+package com.vv.buildstuff.joefresh.responsePlaces;
+
+
+import com.vv.buildstuff.joefresh.LatLong;
+
+/**
+ * Created by vvennava on 10/11/14.
+ */
+public class Geometry {
+    private LatLong location;
+
+    public Geometry() {
+    }
+
+    public Geometry(LatLong location) {
+        this.location = location;
+    }
+
+    public LatLong getLocation() {
+        return location;
+    }
+
+    public void setLocation(LatLong location) {
+        this.location = location;
+    }
+}
